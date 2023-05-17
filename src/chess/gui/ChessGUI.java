9cb7d57f7b892ec720e35a318ca2d67325e9b2fc
jackwrfuller <78133717+jackwrfuller@users.noreply.github.com>
2023-05-17 21:58:17 +1000

    implemented (janky) rank numbering

--- src/chess/gui/ChessGUI.java
+++ src/chess/gui/ChessGUI.java
@@ -14,10 +14,32 @@ import chess.ChessGame;
 public class ChessGUI extends Application {
     ChessGame game;
     VBox root = new VBox();
+
+    HBox boardAndLabels = new HBox();
+    VBox boardAndFileLetters = new VBox();
     BoardGUI board;
     HBox fileLetters = new HBox();
+    VBox rankNumbers = new VBox();
     HBox controls = new HBox();
 
+
+    void drawRankNumbers() {
+        rankNumbers.getChildren().clear();
+        boolean orientation = board.isFlipped;
+        for (int i = 0; i < 8; i++) {
+            Region padding1 = new Region();
+            VBox.setVgrow(padding1, Priority.ALWAYS);
+            Text letter;
+            if (!orientation) {
+                letter = new Text(Integer.toString(8-i));
+            } else {
+                letter = new Text(Integer.toString(i+1));
+            }
+            Region padding2 = new Region();
+            VBox.setVgrow(padding2, Priority.ALWAYS);
+            rankNumbers.getChildren().addAll(padding1, letter, padding2);
+        }
+    }
 
 
     void drawFileLetters() {
@@ -26,7 +48,7 @@ public class ChessGUI extends Application {
         for (int i = 0; i < 8; i++) {
             Region padding1 = new Region();
             HBox.setHgrow(padding1, Priority.ALWAYS);
-            Text letter = new Text();
+            Text letter;
             if (orientation) {
                 letter = new Text(getCharForNumber(7-i));
             } else {
@@ -59,9 +81,9 @@ public class ChessGUI extends Application {
         flipBoard.setOnMouseClicked(e -> {
             board.flipBoard();
             drawFileLetters();
+            drawRankNumbers();
 
         });
-
         controls.getChildren().add(flipBoard);
         controls.setAlignment(Pos.CENTER);
 
@@ -75,11 +97,13 @@ public class ChessGUI extends Application {
         //create and draw elements of application
         game = new ChessGame();
         board = new BoardGUI(game);
-
         drawFileLetters();
+        drawRankNumbers();
+        boardAndFileLetters.getChildren().addAll(board, fileLetters);
+        boardAndLabels.getChildren().addAll(rankNumbers, boardAndFileLetters);
         drawControls();
         // add to root and show
-        root.getChildren().addAll(board, fileLetters, controls);
+        root.getChildren().addAll(boardAndLabels, controls);
         stage.show();
     }
 
